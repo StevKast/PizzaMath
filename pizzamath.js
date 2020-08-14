81f552bcb67df6659503a882bbb1ebfa8f3d1d08
Steven Kast <steven.m.kast@gmail.com>
2020-08-14 01:34:07 -0400

    Add results to table

--- pizzamath.js
+++ pizzamath.js
@@ -1,12 +1,35 @@
 $(document).ready(function () {
 
     $("#pizzaForm").keyup(function () {
-        
-        var diameter = $("#pizzaDiameter").val();
-        $("#pizzaResults").text(calcArea(diameter / 2) + " in/sq");
+
+        var name, cost, diameter, area, inPerDollar;
+
+        name = $("#name").val();
+        cost = $("#cost").val();
+        diameter = $("#pizzaDiameter").val();
+        area = calcArea(diameter / 2);
+        inPerDollar = area / cost;
+        $("#pizzaResults").text(area + " in/sq");
+    });
+
+    $("#submit").click(function () {
+        var name, cost, diameter, area, inPerDollar;
+
+        name = $("#name").val();
+        cost = $("#cost").val();
+        diameter = $("#pizzaDiameter").val();
+        area = calcArea(diameter / 2);
+        inPerDollar = area / cost;
+        addResult(name, cost, diameter, area, inPerDollar);
     });
 
 });
+
+function addResult(name, cost, diameter, area, inPerDollar) {
+    $('table tbody').after('<tr><th scope="row">' +
+        name + '</th><td>' + cost + '</td><td>' + diameter + '</td><td>' +
+        area + '</td><td>' + inPerDollar + '</td></tr>');
+}
 
 function calcArea(radius) {
     return 3.14 * (radius * radius);
